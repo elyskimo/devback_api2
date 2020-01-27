03543f1ed6c203570bf896d75868c29d52e56b8f
Michaela <michell.alfoldiova@gmail.com>
2020-01-27 11:07:13 +0100

    First commit

---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    max: 255
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6
+  }
+});
+
+module.exports = mongoose.model('User',userSchema);
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Import routes
+const authRoutes = require('./routes/auth');
+
+// Connexion BDD
+mongoose.connect(process.env.DB_CONNECT,
+                  { useNewUrlParser: true, useUnifiedTopology: true },
+                  () => console.log("Connected to MongoDB"));
+
+// Middleware
+app.use(express.json());
+
+// Routes middlewares
+app.use('/api/user', authRoutes);
+app.listen(3000, () => console.log("Server up and running on localhost:3000"));
